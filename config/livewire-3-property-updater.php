c13726ff2ce4339538bab6f7d4c2fa8df3056f15
Shawn Veltman <shawn.veltman@gmail.com>
2023-09-30 16:14:12 -0600

    NO-ISSUE fixed issue with config file

--- config/livewire-3-property-updater.php
+++ config/livewire-3-property-updater.php
@@ -2,6 +2,6 @@
 
 // config for Shawnveltman/Livewire3PropertyUpdater
 return [
-    'start_directory' => base_path('app/Livewire'),  // Defaulting to the app directory, but users can change this.
+    'start_directory' => 'app/Livewire',  // Defaulting to the app directory, but users can change this.
     'disk' => 'base_path',
 ];
